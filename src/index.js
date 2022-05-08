d27f494e054a1c188372ee7d6e6dfb92b3c652f3
ol0nny99 <100658149+ol0nny99@users.noreply.github.com>
2022-05-08 00:05:19 -0400

    completes assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form') 
+  form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  createToDo(e.target.querySelector('#new-task-description').value)
+  form.reset()
+ })
+ 
+ })
+ 
+ function createToDo(todo) {
+   let p = document.createElement('p');
+   let btn = document.createElement('button');
+   btn.addEventListener('click', handleDelete);
+   btn.textContent = 'x'
+   p.textContent = `${todo}  `
+   p.appendChild(btn)
+   console.log(p)
+   document.querySelector('#list').appendChild(p)
+ }
+ 
+ function handleDelete(e) {
+ e.target.parentNode.remove()
+ }
